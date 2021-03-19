7de0f733b2006c9ea5395eba7db501efcb392d78
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-19 15:03:32 +0000

    Minor Spaces on CSS Added line 81, JS removed some spaces

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 document.getElementById('facts').addEventListener('click', facts)
 document.getElementById('timeline').addEventListener('click', timeline)
 document.getElementById('home').addEventListener('click', home)
-let answer 
+let answer
 
 function facts () {
-  answer = confirm ('Press OK to Continue to Fun Facts')
-  if  (answer) {
+  answer = confirm('Press OK to Continue to Fun Facts')
+  if (answer) {
   window.location = 'https://assignment-2.jacksonnaufal.repl.co/facts.html'
-} 
+}
 }
 
 function timeline () {
-  answer = confirm ('Press OK to Continue to Timeline')
+  answer = confirm('Press OK to Continue to Timeline')
   if (answer) {
   window.location = 'https://assignment-2.jacksonnaufal.repl.co/Timeline.html'
 }
 }
 
 function home () {
-  answer = confirm ('Press OK to Continue to Home')
+  answer = confirm('Press OK to Continue to Home')
   if (answer) {
   window.location = 'https://assignment-2.jacksonnaufal.repl.co/'
 }
