59be2ce2d5f964060c8ae852fd1cbdf6879e2dd5
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-19 14:58:21 +0000

    Changes in Java Script

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 document.getElementById('facts').addEventListener('click', facts)
 document.getElementById('timeline').addEventListener('click', timeline)
 document.getElementById('home').addEventListener('click', home)
+let answer 
 
 function facts () {
-  let answer = confirm ('Press OK to Continue to Fun Facts')
-  if (answer)
+  answer = confirm ('Press OK to Continue to Fun Facts')
+  if  (answer) {
   window.location = 'https://assignment-2.jacksonnaufal.repl.co/facts.html'
+} 
 }
 
 function timeline () {
-  let answer = confirm ('Press OK to Continue to Timeline')
-  if (answer)
+  answer = confirm ('Press OK to Continue to Timeline')
+  if (answer) {
   window.location = 'https://assignment-2.jacksonnaufal.repl.co/Timeline.html'
+}
 }
 
 function home () {
-  let answer = confirm ('Press OK to Continue to Home')
-  if (answer)
+  answer = confirm ('Press OK to Continue to Home')
+  if (answer) {
   window.location = 'https://assignment-2.jacksonnaufal.repl.co/'
+}
 }
